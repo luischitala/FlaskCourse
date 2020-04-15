0744ea29c668c48d6a99d62f0744963f1a3683d6
luischitala <lrchitala@gmail.com>
2020-04-14 22:02:39 -0500

    Erorhandler

--- venv/main.py
+++ venv/main.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 todos = ['Comprar café', 'Enviar solicitud de compra ', 'Entregar video al productor']
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
