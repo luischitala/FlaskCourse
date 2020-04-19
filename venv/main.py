6a61a087086c0b80d809654190b1e6449dc1ee2d
luischitala <lrchitala@gmail.com>
2020-04-19 14:43:27 -0500

    WTForms added

--- venv/main.py
+++ venv/main.py
@@ -1,8 +1,20 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Comprar café', 'Enviar solicitud de compra ', 'Entregar video al productor']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    sumbit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -13,17 +25,17 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello')) 
-    response.set_cookie('user_ip',user_ip)
-
+    session['user_ip'] = user_ip
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
-
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
          'todos': todos,
+         'login_form': login_form
     }
 
     return render_template('hello.html', **context)
