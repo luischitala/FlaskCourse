fae789b3bb79879124814e834e453745ccd07cd2
luischitala <lrchitala@gmail.com>
2020-04-19 16:19:33 -0500

    FLash

--- venv/main.py
+++ venv/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -28,14 +28,26 @@ def index():
     session['user_ip'] = user_ip
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET','POST'])
 def hello():
     user_ip = session.get('user_ip')
     login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
          'todos': todos,
-         'login_form': login_form
+         'login_form': login_form,
+         'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        
+        flash('Nombre de usuario registrado con éxito')
+
+        return redirect(url_for('index'))
+
 
     return render_template('hello.html', **context)
